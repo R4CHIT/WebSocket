221b82f59713c22a088981322967ff49e899215a
R4CHIT <tiwarirachit50@gmail.com>
2025-07-19 16:30:44 +0545

    first commit

---
+++ ChatApp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Room)  # Register your models here, e.g., admin.site.register(Room), admin.site.register(Message)
+admin.site.register(Message)  # Register your models here, e.g., admin.site.register(Room), admin.site.register(Message)
---
+++ ChatApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+    sender = models.CharField(max_length=100)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.room)
