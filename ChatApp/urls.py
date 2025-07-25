00f275e406d2c366eb1c9e1b182420ed08351bf8
R4CHIT <tiwarirachit50@gmail.com>
2025-07-25 20:22:55 +0545

    Implement room creation and messaging functionality; update templates and views

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import room
+from .views import *
 
 urlpatterns = [
     path('', room, name='home'),
+    path('/message/<str:username>/<str:room_name>', message, name='room'),
 ]
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
 
+from django.shortcuts import render,redirect
+from .models import Room
 # Create your views here.
 def room(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        room_name = request.POST.get('room_id')
+        print(username, room_name)
+        try:
+            getroom = Room.objects.get(name=room_name)
+            print("Room exists:", getroom.name)
+        except  Room.DoesNotExist:
+            new_room = Room(name=room_name)
+            new_room.save()
+            print("New room created:", new_room.name)
+        return redirect('room', username=username, room_name=room_name)
     return render(request, 'room.html')
+
+def message(request,username,room_name):
+    return render(request, 'message.html')
